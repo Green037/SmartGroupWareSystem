f5759cd3648c70856f4cbc206318ba0e87e70c5b
GREEN\EUNBEE <tsuyu040913@naver.com>
2017-02-17 23:23:38 +0900

    2017/02/17 고은비

--- Smart_Groupware_System/src/main/java/com/cafe24/smart/extraSchedule/controller/ExtraScheduleContrller.java
+++
@@ -1,7 +0,0 @@
-package com.cafe24.smart.extraSchedule.controller;
-
-import org.apache.log4j.Logger;
-
-public class ExtraScheduleContrller {
-	final static Logger log = Logger.getLogger(ExtraScheduleContrller.class);
-}
