5493e0f567495a2da81b37479ef9385d9ae0832b
green <tsuyu040913@naver.com>
2017-02-27 14:11:10 +0900

    2017/02/27 고은비 : Insurance

--- Smart_Groupware_System/src/main/java/com/cafe24/smart/insurance/dao/InsuranceDAOImpl.java
+++ Smart_Groupware_System/src/main/java/com/cafe24/smart/insurance/dao/InsuranceDAOImpl.java
@@ -11,18 +11,18 @@ import com.cafe24.smart.insurance.domain.PensionInsurance;
 
 @Repository
 public class InsuranceDAOImpl implements InsuranceDAO {
-
 	@Autowired
 	SqlSessionTemplate sqlSession;
 	
+//	select one EmployeementInsurance by year
 	@Override
 	public EmployeementInsurance selectByEi(int eiYear) {
 		
 		System.out.println("InsuranceDAOImpl selectByEi eiYear : " + eiYear);
 		
-		EmployeementInsurance employeementInsurance = null;
+		EmployeementInsurance employeementInsurance = sqlSession.selectOne("InDAO.selectByEi", eiYear);
 		
-		employeementInsurance = sqlSession.selectOne("InDAO.selectByEi", eiYear);
+		System.out.println("InsuranceDAOImpl selectByEi employeementInsurance : " + employeementInsurance);
 		
 		if (employeementInsurance == null) {
 			sqlSession.rollback();
@@ -31,14 +31,15 @@ public class InsuranceDAOImpl implements InsuranceDAO {
 		return employeementInsurance;
 	}
 
+//	select one NationalHealthInsurance by year
 	@Override
 	public NationalHealthInsurance selectByNhi(int nhiYear) {
 		
 		System.out.println("InsuranceDAOImpl selectByNhi nhiYear : " + nhiYear);
 		
-		NationalHealthInsurance nationalHealthInsurance = null;
+		NationalHealthInsurance nationalHealthInsurance = sqlSession.selectOne("InDAO.selectByNhi", nhiYear);
 		
-		nationalHealthInsurance = sqlSession.selectOne("InDAO.selectByNhi", nhiYear);
+		System.out.println("InsuranceDAOImpl selectByNhi nationalHealthInsurance : " + nationalHealthInsurance);
 		
 		if (nationalHealthInsurance == null) {
 			sqlSession.rollback();
@@ -47,14 +48,15 @@ public class InsuranceDAOImpl implements InsuranceDAO {
 		return nationalHealthInsurance;
 	}
 
+//	select one OccupationalHealthSafetyInsurance by year
 	@Override
 	public OccupationalHealthSafetyInsurance selectByOhi(int ohiYear) {
 		
 		System.out.println("InsuranceDAOImpl selectByOhi ohiYear : " + ohiYear);
 		
-		OccupationalHealthSafetyInsurance occupationalHealthSafetyInsurance = null;
+		OccupationalHealthSafetyInsurance occupationalHealthSafetyInsurance = sqlSession.selectOne("InDAO.selectByOhi", ohiYear);
 		
-		occupationalHealthSafetyInsurance = sqlSession.selectOne("InDAO.selectByOhi", ohiYear);
+		System.out.println("InsuranceDAOImpl selectByOhi occupationalHealthSafetyInsurance : " + occupationalHealthSafetyInsurance);
 		
 		if (occupationalHealthSafetyInsurance == null) {
 			sqlSession.rollback();
@@ -63,14 +65,15 @@ public class InsuranceDAOImpl implements InsuranceDAO {
 		return occupationalHealthSafetyInsurance;
 	}
 
+//	select one PensionInsurance by year
 	@Override
 	public PensionInsurance selectByPp(int ppYear) {
 		
 		System.out.println("InsuranceDAOImpl selectByPp ppYear : " + ppYear);
 		
-		PensionInsurance pensionInsurance = null;
+		PensionInsurance pensionInsurance = sqlSession.selectOne("InDAO.selectByPp", ppYear);
 		
-		pensionInsurance = sqlSession.selectOne("InDAO.selectByPp", ppYear);
+		System.out.println("InsuranceDAOImpl selectByPp pensionInsurance : " + pensionInsurance);
 		
 		if (pensionInsurance == null) {
 			sqlSession.rollback();
--- Smart_Groupware_System/src/main/java/com/cafe24/smart/payment/controller/PaymentController.java
+++ Smart_Groupware_System/src/main/java/com/cafe24/smart/payment/controller/PaymentController.java
@@ -1,14 +1,24 @@
 package com.cafe24.smart.payment.controller;
 
+
+import java.util.Calendar;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.cafe24.smart.insurance.service.InsuranceService;
 
 @Controller
 public class PaymentController {
 	private static final Logger log = LoggerFactory.getLogger(PaymentController.class);
+	
+	@Autowired
+	InsuranceService insuranceService;
 	
 	@RequestMapping(value = "pc/list", method = RequestMethod.GET)
 	public String pcListCtrl() {
@@ -29,8 +39,21 @@ public class PaymentController {
 	}
 	
 	@RequestMapping(value = "pa/add", method = RequestMethod.GET)
-	public String paAddCtrl() {
-						
+	public String paAddCtrl(Model model) {	
+		
+		Calendar calendar = Calendar.getInstance();
+		
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH) + 1;
+		
+		model.addAttribute("eiContent", insuranceService.eiContentServ(year));
+		model.addAttribute("nhiContent", insuranceService.nhiContentServ(year));
+		model.addAttribute("ppContent", insuranceService.ppContentServ(year));
+		model.addAttribute("month", month);
+		
+		System.out.println("PaymentController paAddCtrl model : " + model);
+		System.out.println("PaymentController paAddCtrl month : " + month);
+		
 		return "payment/pa_add";
 	}
 }
